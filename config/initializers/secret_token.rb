5657db3136709c3f2b6f41be4542c2d2d9d99006
Diogo Castro <diogo.filipe.acastro@gmail.com>
2012-06-13 09:39:58 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyWineServer::Application.config.secret_token = '69ff26b7add24a08fe3af4a1247fa47e7173d282ef19163ea2f7d9ace06d9d4de270544582c7c8a9bf2354daaf07de9043b011dec57a6ec483edc32022c36962'
